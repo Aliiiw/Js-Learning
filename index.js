ee2fcbee0d927b5c70b057ef83b4e5f820216565
Ali Rahimi <ali.rahimi@gandom.co>
2024-08-19 18:29:15 +0330

    variables

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+let myName = "name";
+let firstName, lastName;
+let var1;
+let var2;
+const myConst = 5;
+myConst = 7; //error
+console.log(myName);
+
+//primitives
+let name = "ali";
+let age = 22;
+let isOk = true;
+let price = 12.5;
+let test; //undefined
+let testNull = null;
+let test2 = undefined;
