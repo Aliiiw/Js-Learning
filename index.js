8f18471f782b06c5662ee9602412c4b52603f231
Ali Rahimi <ali.rahimi@gandom.co>
2024-08-19 18:35:07 +0330

    objects

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-let myName = "name";
-let firstName, lastName;
-let var1;
-let var2;
-const myConst = 5;
-myConst = 7; //error
-console.log(myName);
+// Objects
 
-//primitives
 let name = "ali";
 let age = 22;
-let isOk = true;
-let price = 12.5;
-let test; //undefined
-let testNull = null;
-let test2 = undefined;
+
+let person = {
+  name: "ali",
+  age: 22
+};
+
+console.log(person.name);
+console.log(person["name"]);
